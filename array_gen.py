bf7861a4401426b82c4106b421038a7f02c6ede4
Sergey Gorohov <gamehouse02@yandex.ru>
2025-02-14 11:25:27 +0300

    Fixed array_gen

--- array_gen.py
+++ array_gen.py
@@ -1,14 +1,8 @@
 import numpy as np
 
 
-N = 5
+N = 10
 
-arr = np.zeros((N, N))
-
-di = np.diag_indices(N)
-
-di2 = ((di[0] + 1)[:N - 1], np.flip(di[1])[:N - 1])
-
-arr[di2] = np.arange(N - 1) + 1
+arr = np.rot90(np.diagflat(np.arange(N - 1, 0, -1), -1))
 
 print(arr)
